6e6d6019a28127c10ab766f0f8d6f44e8527e7eb
Flavio Pinto <flaviopnt@hotmail.it>
2020-04-16 15:21:43 +0200

    html/css/js files created

---
+++ js/main.js
@@ -0,0 +1,5 @@
+/**
+ * Ricreare la todo list vista oggi durante la lezione (focus dell’esercizio la logica)
+ * Usiamo i template in html e il clone per separare la parte logica (JS) dalla parte di markup (HTML)
+ * Recap funzionalità: mostrare, aggiungere e eliminare elementi dalla lista
+ */
